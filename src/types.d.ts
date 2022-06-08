211cd2800c4de77273d742fc35821f3603800fdb
Lázaro <lazaroyaesta@gmail.com>
2022-06-08 16:15:31 +0200

    Fix with lint

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,12 +1,12 @@
-export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy';
-export type Visibility = 'greate' | 'good' | 'ok' | 'poor';
+export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy'
+export type Visibility = 'greate' | 'good' | 'ok' | 'poor'
 
 export interface DiaryEntry {
-    id: number;
-    date: string;
-    weather: Weather;
-    visibility: Visibility;
-    comment: string;
+  id: number
+  date: string
+  weather: Weather
+  visibility: Visibility
+  comment: string
 }
 
 // las interfaces pueden extender, los types no
