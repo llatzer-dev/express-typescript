1589d0376b6a5016eedd8d72f62028a52690af9c
Lázaro <lazaroyaesta@gmail.com>
2022-06-08 17:19:19 +0200

    Create FindById path & service

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -7,6 +7,14 @@ router.get('/', (_req, res) => {
   res.send(diaryServices.getEntriesWithoutSensitiveInfo())
 })
 
+router.get('/:id', (_req, res) => {
+  const diary = diaryServices.findById(+_req.params.id) // el '+' es para que detecte que es un number, y no un string
+
+  return (diary != null)
+    ? res.send(diary)
+    : res.sendStatus(404)
+})
+
 router.post('/', (_req, res) => {
   res.send('Saving a diary!')
 })
--- src/services/diaryServices.ts
+++ src/services/diaryServices.ts
@@ -5,6 +5,16 @@ const diaries: DiaryEntry[] = diaryData as DiaryEntry[]
 
 export const getEntries = (): DiaryEntry[] => diaries
 
+export const findById = (id: number): NonSensitiveInfoDiaryEntry | undefined => {
+  const entry = diaries.find(d => d.id === id)
+  if (entry != null) {
+    const { comment, ...restOfDiary } = entry
+    return restOfDiary
+  }
+
+  return undefined
+}
+
 export const getEntriesWithoutSensitiveInfo = (): NonSensitiveInfoDiaryEntry[] => {
   return diaries.map(({ id, date, weather, visibility }) => {
     return {
