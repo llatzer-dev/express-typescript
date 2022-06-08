630b6ebb5f5dd0eee642b40a1891766bb58b07ce
Lázaro <lazaroyaesta@gmail.com>
2022-06-08 16:11:25 +0200

    Install Lint & lint script

--- src/services/diaryServices.ts
+++ src/services/diaryServices.ts
@@ -1,8 +1,8 @@
-import { DiaryEntry } from '../types';
-import diaryData from './diaries.json';
+import { DiaryEntry } from '../types'
+import diaryData from './diaries.json'
 
-const diaries: Array<DiaryEntry> = diaryData as Array<DiaryEntry>;
+const diaries: DiaryEntry[] = diaryData as DiaryEntry[]
 
-export const getEntries = () => diaries;
+export const getEntries = (): DiaryEntry[] => diaries
 
-export const addEntry = () => null;
+export const addEntry = (): undefined => undefined
